7cf26ed991442d43957395a72160319f35073be0
Jurayevkh <jurayevkh@gmail.com>
2024-06-24 23:46:09 +0500

    Abstraction and Dependency Injection

---
+++ Resume-Builder.Application/Abstractions/IApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Resume_Builder.Domain.Entities.Resume;
+using Resume_Builder.Domain.Entities.User;
+
+namespace Resume_Builder.Application.Abstractions;
+
+public interface IApplicationDbContext
+{
+    public DbSet<Resumes> Resumes { get; set; }
+    public DbSet<Users> Users { get; set; }
+
+    public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default); 
+}
+
