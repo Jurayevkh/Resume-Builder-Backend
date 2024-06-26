9f7f18855339efcb05d6ffa5e4bcd3c7403c17ae
Jurayevkh <jurayevkh@gmail.com>
2024-06-26 20:48:49 +0500

    Mapper Configuration

---
+++ Resume-Builder.Application/Mappers/MappingConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Resume_Builder.Application.DTO;
+using Resume_Builder.Application.UseCases.Resumes.Commands;
+using Resume_Builder.Application.UseCases.Users.Commands;
+
+
+namespace Resume_Builder.Application;
+
+public class MappingConfiguration:Profile
+{
+    public MappingConfiguration(){
+        CreateMap<CreateUserCommand , CreateUserDTO>().ReverseMap();
+        CreateMap<UpdateUserCommand , UpdateUserDTO>().ReverseMap();
+
+        CreateMap<CreateResumeCommand, CreateResumeDTO>().ReverseMap();
+        CreateMap<UpdateResumeCommand, UpdateResumeDTO>().ReverseMap();
+    }
+}
