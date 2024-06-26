8ce6314713ef0beb72caa765040f59b7bab8a966
Jurayevkh <jurayevkh@gmail.com>
2024-06-26 20:48:37 +0500

    Controllers

---
+++ Resume-Builder.API/Controllers/ResumeController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Resume_Builder.Application.DTO;
+using Resume_Builder.Application.UseCases.Resumes.Commands;
+using Resume_Builder.Application.UseCases.Resumes.Queries;
+
+namespace Resume_Builder.API;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class ResumeController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    private readonly IMapper _mapper;
+
+    public ResumeController(IMediator mediator, IMapper mapper)
+    {
+        _mediator = mediator;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async ValueTask<IActionResult> GetAllResumes()
+    {
+        var resumes = _mediator.Send(new GetAllResumesQuery());
+        return Ok(resumes);
+    }
+
+    [HttpGet]
+    public async ValueTask<IActionResult> GetResumeById(int Id)
+    {
+        var resume = _mediator.Send(new GetResumeByIdQuery() { Id = Id });
+        return Ok(resume);
+    }
+
+    [HttpPost]
+    public async ValueTask<IActionResult> CreateResume(CreateResumeDTO dto)
+    {
+        var resume = _mapper.Map<CreateResumeCommand>(dto);
+        var result = await _mediator.Send(resume);
+        return Ok(result);
+    }
+
+    [HttpPut]
+    public async ValueTask<IActionResult> UpdateResume(UpdateResumeDTO dto)
+    {
+        var resume = _mapper.Map<UpdateResumeCommand>(dto);
+        var result = await _mediator.Send(resume);
+        return Ok(result);
+    }
+
+    [HttpDelete]
+    public async ValueTask<IActionResult> DeleteResume(int Id)
+    {
+        var result = _mediator.Send(new DeleteResumeCommand() { Id = Id });
+        return Ok(result);
+    }
+}
---
+++ Resume-Builder.API/Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Resume_Builder.Application.DTO;
+using Resume_Builder.Application.UseCases.Users.Commands;
+using Resume_Builder.Application.UseCases.Users.Queries;
+
+namespace Resume_Builder.API;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class UserController:ControllerBase
+{
+    private readonly IMediator _mediator;
+    private readonly IMapper _mapper;
+
+    public UserController(IMediator mediator, IMapper mapper)
+    {
+        _mediator = mediator;
+        _mapper = mapper;
+    }
+
+   [HttpGet]
+    public async ValueTask<IActionResult> GetAllUsers()
+    {
+        var users = _mediator.Send(new GetAllUsersQuery());
+        return Ok(users);
+    }
+
+    [HttpGet]
+    public async ValueTask<IActionResult> GetUserById(int Id)
+    {
+        var user = _mediator.Send(new GetUserByIdQuery() { Id = Id });
+        return Ok(user);
+    }
+
+    [HttpPost]
+    public async ValueTask<IActionResult> CreateUser(CreateUserDTO dto)
+    {
+        var user = _mapper.Map<CreateUserCommand>(dto);
+        var result = await _mediator.Send(user);
+        return Ok(result);
+    }
+
+    [HttpPut]
+    public async ValueTask<IActionResult> UpdateUser(UpdateUserDTO dto)
+    {
+        var user = _mapper.Map<UpdateUserCommand>(dto);
+        var result = await _mediator.Send(user);
+        return Ok(result);
+    }
+
+    [HttpDelete]
+    public async ValueTask<IActionResult> DeleteUser(string Email)
+    {
+        var result = _mediator.Send(new DeleteUserCommand() { Email = Email });
+        return Ok(result);
+    }
+
+}
